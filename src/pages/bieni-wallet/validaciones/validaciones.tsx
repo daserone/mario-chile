f4640292eca90f797e35c0977c2dfa7ab240f087
Daniel Ramirez <59937443+daserone@users.noreply.github.com>
2024-02-27 17:13:56 -0400

    estilos pantallas validacion

--- src/pages/bieni-wallet/validaciones/validaciones.tsx
+++ src/pages/bieni-wallet/validaciones/validaciones.tsx
@@ -5,6 +5,13 @@ import Tabs from "react-bootstrap/Tabs";
 import Manual from "./manual/Manual";
 import Dependiente from "./dependiente/Dependiente";
 import Correo from "./correo/Correo";
+import hammer from "@src/assets/icons/hammer.svg";
+import hammerActive from "@src/assets/icons/hammer-active.svg";
+import lego from "@src/assets/icons/lego.svg";
+import legoActive from "@src/assets/icons/lego-active.svg";
+import mail from "@src/assets/icons/mail.svg";
+import mailActive from "@src/assets/icons/mail-active.svg";
+import "./Validaciones.scss";
 
 const Validacion = () => {
   const [key, setKey] = useState("manual");
@@ -15,25 +22,59 @@ const Validacion = () => {
         <Col>
           <h2 className="my-2">BieniWallet/Validación</h2>
           <Card>
-            <Card.Body>
-              <Tabs
-                id="controlled-tab-example"
-                activeKey={key}
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                onSelect={(k: any) => setKey(k)}
-                className="mb-3"
+            <Tabs
+              id="controlled-tab-example"
+              activeKey={key}
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              onSelect={(k: any) => setKey(k)}
+              className="mb-2"
+            >
+              <Tab
+                eventKey="manual"
+                title={
+                  <span>
+                    <img
+                      src={key == "manual" ? hammerActive : hammer}
+                      alt="manual-icon"
+                      className="me-1"
+                    />
+                    Manual
+                  </span>
+                }
               >
-                <Tab eventKey="manual" title="Manual">
-                  <Manual />
-                </Tab>
-                <Tab eventKey="dependiente" title="Dependientes">
-                  <Dependiente />
-                </Tab>
-                <Tab eventKey="correo" title="Verificación de correo">
-                  <Correo />
-                </Tab>
-              </Tabs>
-            </Card.Body>
+                <Manual />
+              </Tab>
+              <Tab
+                eventKey="dependiente"
+                title={
+                  <span>
+                    <img
+                      src={key == "dependiente" ? legoActive : lego}
+                      alt="dependiente-icon"
+                      className="me-1"
+                    />
+                    Dependiente
+                  </span>
+                }
+              >
+                <Dependiente />
+              </Tab>
+              <Tab
+                eventKey="correo"
+                title={
+                  <span>
+                    <img
+                      src={key == "correo" ? mailActive : mail}
+                      alt="correo-icon"
+                      className="me-1"
+                    />
+                    Verificación de correo
+                  </span>
+                }
+              >
+                <Correo />
+              </Tab>
+            </Tabs>
           </Card>
         </Col>
       </Row>
